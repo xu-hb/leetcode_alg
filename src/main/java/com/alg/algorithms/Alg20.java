f07b4fee7fa55e3810586e1453e1e2effe6436cb
prysnail <prysnail@outlook.com>
2022-03-21 23:50:23 +0800

    012.整数转罗马数字

--- src/main/java/com/alg/algorithms/Alg20.java
+++ src/main/java/com/alg/algorithms/Alg20.java
@@ -44,4 +44,22 @@ public class Alg20 {
         return maxArea;
     }
 
+    /**
+     * 12.整数转罗马数字
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        int[] values = new int[]{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i=0; i<values.length; i++){
+            while (num>=values[i]){
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
