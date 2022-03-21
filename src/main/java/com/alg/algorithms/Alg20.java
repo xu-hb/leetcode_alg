fbed4e23194ee943eece307f647c586e3bcb1fe7
prysnail <prysnail@outlook.com>
2022-03-21 23:50:54 +0800

    013.罗马数字转整数

--- src/main/java/com/alg/algorithms/Alg20.java
+++ src/main/java/com/alg/algorithms/Alg20.java
@@ -1,5 +1,8 @@
 package com.alg.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 算法11-20
  */
@@ -62,4 +65,34 @@ public class Alg20 {
         }
         return sb.toString();
     }
+
+    /**
+     * 13.罗马数字转整数
+     * @param s
+     * @return
+     */
+    public int romanToInt(String s) {
+        Map<Character , Integer> map = new HashMap<>();
+        map.put('I' , 1);
+        map.put('V' , 5);
+        map.put('X' , 10);
+        map.put('L' , 50);
+        map.put('C' , 100);
+        map.put('D' , 500);
+        map.put('M' , 1000);
+
+        int result = 0;
+        for (int i=0;i<s.length()-1;i++){
+            int v1 = map.get(s.charAt(i));
+            int v2 = map.get(s.charAt(i+1));
+            if (v1<v2){
+                result -=v1;
+            }else {
+                result +=v1;
+            }
+        }
+        result += map.get(s.charAt(s.length()-1));
+        return result;
+    }
+
 }
