443ae662f1ac4af83c819e3712d233b69ec32c7d
prysnail <prysnail@outlook.com>
2022-04-05 15:30:33 +0800

    977.有序的数组的平方

---
+++ src/main/java/com/alg/algorithms/alg1000/Alg980.java
@@ -0,0 +1,27 @@
+package com.alg.algorithms.alg1000;
+
+public class Alg980 {
+    /**
+     * 977.有序的数组的平方
+     * @param nums
+     * @return
+     */
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int low=0;
+        int high=nums.length-1;
+        int cursor = nums.length-1;
+
+        while (low<=high){
+            if (Math.abs(nums[low])<Math.abs(nums[high])){
+                result[cursor]=nums[high] * nums[high];
+                high--;
+            }else {
+                result[cursor]=nums[low] * nums[low];
+                low++;
+            }
+            cursor--;
+        }
+        return result;
+    }
+}
