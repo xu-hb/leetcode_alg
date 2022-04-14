438d54ecbb9c309f5ce13f91f1174ae02dd847d1
prysnail <prysnail@outlook.com>
2022-04-14 16:52:56 +0800

    144.二叉树的前序遍历 2

--- src/main/java/com/alg/algorithms/alg200/Alg150.java
+++ src/main/java/com/alg/algorithms/alg200/Alg150.java
@@ -7,8 +7,9 @@ import java.util.*;
 public class Alg150 {
     /**
      * 144.二叉树的前序遍历
+     * 递归
      * 时间复杂度：O(N)
-     * 空间复杂度：O(1)
+     * 空间复杂度：O(N) 递归栈深度N
      * @param root
      * @return
      */
@@ -33,4 +34,29 @@ public class Alg150 {
         traversal(node.right , list);
     }
 
+    /**
+     * 144.二叉树的前序遍历 2
+     * 迭代，本质上合递归一样，不过将栈置于明处
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal_2(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        TreeNode node = root;
+        //栈
+        Deque<TreeNode> stack = new LinkedList<>();
+        while (!stack.isEmpty() || node!=null){
+            while (node !=null){
+                result.add(node.val);
+                stack.push(node);
+                node=node.left;
+            }
+            node=stack.pop().right;
+        }
+        return result;
+    }
 }
