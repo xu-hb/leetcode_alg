d480d8c0a279c3c52495ad72ae48e28ae6e2416e
prysnail <prysnail@outlook.com>
2022-05-19 11:15:40 +0800

    198.打家劫舍:DP

--- src/main/java/com/alg/algorithms/alg200/Alg200.java
+++ src/main/java/com/alg/algorithms/alg200/Alg200.java
@@ -9,6 +9,29 @@ import java.util.Queue;
 
 public class Alg200 {
     /**
+     * 198.打家劫舍
+     * DP
+     * @param nums
+     * @return
+     */
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        //dp:到第i家抢劫的金额
+        int[] dp = new int[nums.length];
+        //初始化
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            //递推公式
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[nums.length - 1];
+    }
+
+    /**
      * 199.二叉树的右视图
      * @param root
      * @return
