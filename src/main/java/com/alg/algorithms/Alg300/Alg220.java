3e40d0bec1f0aa3b41ffac0883d502f2c0891a57
prysnail <prysnail@outlook.com>
2022-06-24 15:42:58 +0800

    213.打家劫舍II

--- src/main/java/com/alg/algorithms/Alg300/Alg220.java
+++ src/main/java/com/alg/algorithms/Alg300/Alg220.java
@@ -6,6 +6,31 @@ import java.util.List;
 public class Alg220 {
     List<List<Integer>> resultList = new ArrayList<>();
     List<Integer> list = new ArrayList<>();
+
+    /**
+     * 213.打家劫舍II
+     * @param nums
+     * @return
+     */
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int len = nums.length;
+        if (len == 1)
+            return nums[0];
+        return Math.max(robAction(nums, 0, len - 1), robAction(nums, 1, len));
+    }
+
+    int robAction(int[] nums, int start, int end) {
+        int x = 0, y = 0, z = 0;
+        for (int i = start; i < end; i++) {
+            y = z;
+            z = Math.max(y, x + nums[i]);
+            x = y;
+        }
+        return z;
+    }
+
     /**
      * 216.组合总和III
      * 回溯
