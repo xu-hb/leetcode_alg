a34c9088aa8c3fe34affc816dae51cb763a6d557
prysnail <prysnail@outlook.com>
2022-07-05 14:13:03 +0800

    86.分隔链表：双指针

--- src/main/java/com/alg/algorithms/alg100/Alg90.java
+++ src/main/java/com/alg/algorithms/alg100/Alg90.java
@@ -1,12 +1,52 @@
 package com.alg.algorithms.alg100;
 
+import com.alg.common.ListNode;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Alg90 {
     List<Integer> list = new ArrayList<>();
     List<List<Integer>> result = new ArrayList<>();
+
+    /**
+     * 86.分隔链表
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     * @param head
+     * @param x
+     * @return
+     */
+    public ListNode partition(ListNode head, int x) {
+        if (Objects.isNull(head)){
+            return null;
+        }
+        ListNode dummyHead = new ListNode();
+        ListNode bigHead = new ListNode();
+
+        ListNode curDummy = dummyHead;
+        ListNode curBig = bigHead;
+
+        ListNode cursor = head;
+        while (cursor !=null){
+            //记录下一个节点
+            ListNode tempNode = cursor.next;
+            if (cursor.val<x){
+                curDummy.next = cursor;
+                curDummy = curDummy.next;
+                curDummy.next=null;
+            }else {
+                curBig.next = cursor;
+                curBig = curBig.next;
+                curBig.next = null;
+            }
+            cursor = tempNode;
+        }
+        curDummy.next = bigHead.next;
+        return dummyHead.next;
+    }
 
     /**
      * 90.子集II
