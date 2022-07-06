9c916ea12b03284667456400a8a55e034e007b3f
prysnail <prysnail@outlook.com>
2022-07-06 14:53:50 +0800

    303.区域和检索-数组不可变 2_前缀和

--- src/main/java/com/alg/algorithms/alg400/Alg310.java
+++ src/main/java/com/alg/algorithms/alg400/Alg310.java
@@ -4,9 +4,9 @@ public class Alg310 {
 
     int[] nums =null;
 
-    public Alg310(int[] nums) {
-        this.nums = nums;
-    }
+//    public Alg310(int[] nums) {
+//        this.nums = nums;
+//    }
     /**
      * 303.区域和检索-数组不可变
      * 时间复杂度：O(N)
@@ -21,4 +21,25 @@ public class Alg310 {
         }
         return sum;
     }
+
+    /**
+     * 303.区域和检索-数组不可变 II
+     * 前缀和，提前求和
+     * 时间复杂度:O(1)
+     * @param left
+     * @param right
+     * @return
+     */
+    public int sumRange_2(int left, int right){
+        return nums[right+1] - nums[left];
+    }
+
+    public Alg310(int[] n) {
+        //计算和
+        this.nums = new int[n.length+1];
+        for (int i=1;i<nums.length;i++){
+            nums[i] = nums[i-1] + n[i-1];
+        }
+    }
+
 }
