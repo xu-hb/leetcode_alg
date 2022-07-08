d39990dc0c4e0ff4984747440109a678b31a8b99
prysnail <prysnail@outlook.com>
2022-07-08 17:09:18 +0800

    875.爱吃香蕉的可可

--- src/main/java/com/alg/algorithms/alg900/Alg880.java
+++ src/main/java/com/alg/algorithms/alg900/Alg880.java
@@ -2,7 +2,38 @@ package com.alg.algorithms.alg900;
 
 import com.alg.common.ListNode;
 
+import java.util.Arrays;
+
 public class Alg880 {
+
+    /**
+     * 875.爱吃香蕉的可可
+     * @param piles
+     * @param h
+     * @return
+     */
+    public int minEatingSpeed(int[] piles, int h) {
+        //吃香蕉速度下限
+        int low = 1;
+        //吃香蕉速度上限
+        int high = Arrays.stream(piles).max().getAsInt();
+
+        while (low<=high){
+            long needHour = 0;
+            int middle = low+(high-low)/2;
+            for (int p : piles){
+                //向上取整
+                needHour += (p + middle - 1) / middle;
+            }
+            if (needHour>h){
+                low=middle+1;
+            }else {
+                high=middle-1;
+            }
+        }
+        return low;
+    }
+
     /**
      * 876.链表中间的节点
      * @param head
