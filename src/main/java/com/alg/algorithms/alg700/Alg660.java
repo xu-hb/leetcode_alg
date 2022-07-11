9d338dd3afc6e1f5b53078345aae82003e0b4e3e
prysnail <prysnail@outlook.com>
2022-07-11 22:09:50 +0800

    652.寻找重复的子树

--- src/main/java/com/alg/algorithms/alg700/Alg660.java
+++ src/main/java/com/alg/algorithms/alg700/Alg660.java
@@ -2,7 +2,38 @@ package com.alg.algorithms.alg700;
 
 import com.alg.common.TreeNode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Alg660 {
+
+    Map<String,Integer> count;
+    List<TreeNode> result;
+    /**
+     * 652.寻找重复的子树
+     * @param root
+     * @return
+     */
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        count = new HashMap<>();
+        result = new ArrayList<>();
+        collect(root);
+        return result;
+    }
+    private String collect(TreeNode node){
+        if (node==null) return "#";
+        //通过路径形成key，判定是否有相同结构和节点值
+        String key = node.val+","+collect(node.left)+","+collect(node.right);
+        count.put(key,count.getOrDefault(key,0)+1);
+        if (count.get(key)==2){
+            //重复的不纳入
+            result.add(node);
+        }
+        return key;
+    }
+
     /**
      * 654.最大二叉树
      * @param nums
