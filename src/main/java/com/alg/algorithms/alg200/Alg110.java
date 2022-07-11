4e4f04abaaba7ab7b302f02462b16da328fe99c4
prysnail <prysnail@outlook.com>
2022-07-11 10:44:16 +0800

    106.从中序和后序遍历序列构造二叉树

--- src/main/java/com/alg/algorithms/alg200/Alg110.java
+++ src/main/java/com/alg/algorithms/alg200/Alg110.java
@@ -182,6 +182,53 @@ public class Alg110 {
     }
 
     /**
+     * 106.从中序和后序遍历序列构造二叉树
+     * @param inorder
+     * @param postorder
+     * @return
+     */
+    public TreeNode buildTree_(int[] inorder, int[] postorder) {
+        inorderMap = new HashMap<>(inorder.length);
+        for (int i=0;i<inorder.length;i++){
+            inorderMap.put(inorder[i],i);
+        }
+        return build(inorder,0,inorder.length-1,
+                postorder,0,postorder.length-1);
+    }
+
+    /**
+     * 从中序和后序中构建二叉树
+     * @param inStart 中序开始
+     * @param inEnd 中序结束
+     * @param postStart 后序开始
+     * @param postEnd 后序结束
+     * @return
+     */
+    TreeNode build(int[] inorder, int inStart, int inEnd,
+                   int[] postorder, int postStart, int postEnd) {
+
+        if (inStart > inEnd) {
+            return null;
+        }
+        // root 节点对应的值就是后序遍历数组的最后一个元素
+        int rootVal = postorder[postEnd];
+        // rootVal 在中序遍历数组中的索引
+        int index = inorderMap.get(rootVal);
+        // 左子树的节点个数
+        int leftSize = index - inStart;
+        TreeNode root = new TreeNode(rootVal);
+        // 递归构造左右子树
+        root.left = build(inorder, inStart, index - 1,
+                postorder, postStart, postStart + leftSize - 1);
+
+        root.right = build(inorder, index + 1, inEnd,
+                postorder, postStart + leftSize, postEnd - 1);
+        return root;
+    }
+
+
+
+    /**
      * 107.二叉树的层序遍历two
      * @param root
      * @return
