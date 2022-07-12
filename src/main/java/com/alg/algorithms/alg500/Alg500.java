eaab9f8b31effee82142333761f39924b36f8ae0
prysnail <prysnail@outlook.com>
2022-07-12 20:13:26 +0800

    493.翻转对 2.0--部分

--- src/main/java/com/alg/algorithms/alg500/Alg500.java
+++ src/main/java/com/alg/algorithms/alg500/Alg500.java
@@ -1,5 +1,9 @@
 package com.alg.algorithms.alg500;
 
+import com.alg.common.Pair;
+
+import java.util.Arrays;
+
 public class Alg500 {
     /**
      * 493.翻转对
@@ -7,7 +11,6 @@ public class Alg500 {
      * @param nums
      * @return
      */
-    //给定一个数组 nums ，如果 i < j 且 nums[i] > 2*nums[j] 我们就将 (i, j) 称作一个重要翻转对。
     public int reversePairs(int[] nums) {
         int result = 0;
         for (int i=0;i<nums.length;i++){
@@ -18,5 +21,37 @@ public class Alg500 {
             }
         }
         return result;
+    }
+
+    /**
+     * 493.翻转对 2.0
+     * @param nums
+     * @return
+     */
+    public int reversePairs_2(int[] nums) {
+        count = new int[nums.length];
+        temps = new Pair[nums.length];
+
+        Pair[] arr = new Pair[nums.length];
+        for (int i=0;i< nums.length;i++){
+            temps[i]=new Pair(nums[i],i);
+        }
+        sort(arr , 0 , nums.length-1);
+        return Arrays.stream(count).sum();
+    }
+
+    int[] count =null;
+    Pair[] temps = null;
+
+    private void sort(Pair[] arr, int start, int end) {
+        if (start==end)
+            return;
+        int middle = (end-start)/2 + start;
+        sort(arr,start,middle);
+        sort(arr,middle+1,end);
+        merge(arr,start,middle,end);
+    }
+    private void merge(Pair[] arr, int start, int middle, int end){
+
     }
 }
