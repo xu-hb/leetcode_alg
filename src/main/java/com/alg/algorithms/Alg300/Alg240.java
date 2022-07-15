9d4e4e8489238cad39ab6a5b29a28703244f9c29
prysnail <prysnail@outlook.com>
2022-07-15 15:00:10 +0800

    236.二叉树的最近公共祖先

--- src/main/java/com/alg/algorithms/Alg300/Alg240.java
+++ src/main/java/com/alg/algorithms/Alg300/Alg240.java
@@ -2,6 +2,9 @@ package com.alg.algorithms.Alg300;
 
 import com.alg.common.TreeNode;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Alg240 {
     /**
      * 235.二叉搜索时的最近公共祖先
@@ -30,5 +33,27 @@ public class Alg240 {
             }
         }
         return null;
+    }
+
+    /**
+     * 236.二叉树的最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor_2(TreeNode root, TreeNode p, TreeNode q) {
+        location(root , p.val , q.val);
+        return result;
+    }
+    TreeNode result = null;
+    private boolean location(TreeNode root , int pVal , int qVal) {
+        if (root==null) return false;
+        boolean leftFlag = location(root.left , pVal , qVal);
+        boolean rightFlag = location(root.right , pVal , qVal);
+        if ((leftFlag && rightFlag)||(root.val==pVal||root.val==qVal)&&(leftFlag || rightFlag)){
+            result=root;
+        }
+        return leftFlag || rightFlag || root.val==pVal || root.val==qVal;
     }
 }
