7afc2305bcefc2579e17dfd01a630199ec3ee568
prysnail <prysnail@outlook.com>
2022-07-15 11:28:17 +0800

    235.二叉搜索时的最近公共祖先

---
+++ src/main/java/com/alg/algorithms/Alg300/Alg240.java
@@ -0,0 +1,34 @@
+package com.alg.algorithms.Alg300;
+
+import com.alg.common.TreeNode;
+
+public class Alg240 {
+    /**
+     * 235.二叉搜索时的最近公共祖先
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int pVal = p.val;
+        int qVal = q.val;
+        min = Math.min(pVal,qVal);
+        max = Math.max(pVal,qVal);
+        return search(root);
+    }
+    int min = 0;
+    int max = 0;
+    private TreeNode search(TreeNode root){
+        while (root !=null){
+            if (root.val>=min && root.val<=max){
+                return root;
+            }else if (max<= root.val){
+                return search(root.left);
+            }else if (min>= root.val){
+                return search(root.right);
+            }
+        }
+        return null;
+    }
+}
