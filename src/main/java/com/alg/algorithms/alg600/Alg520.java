10860feaa397351ce3c3166018a62dedf851c5fd
prysnail <prysnail@outlook.com>
2022-07-20 10:19:51 +0800

    516.最长回文子序列 2.0--转为最长公共子序列问题

--- src/main/java/com/alg/algorithms/alg600/Alg520.java
+++ src/main/java/com/alg/algorithms/alg600/Alg520.java
@@ -1,5 +1,7 @@
 package com.alg.algorithms.alg600;
 
+import com.alg.algorithms.alg1200.Alg1150;
+
 public class Alg520 {
     /**
      * 516.最长回文子序列
@@ -25,5 +27,18 @@ public class Alg520 {
             }
         }
         return dp[0][n - 1];
+    }
+
+    /**
+     * 516.最长回文子序列 2.0
+     * 转为最长公共子序列问题
+     * @param s
+     * @return
+     */
+    public int longestPalindromeSubseq_2(String s){
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        //字符串&字符串翻转，转换为最长公共子序列问题
+        return new Alg1150().longestCommonSubsequence(s , sb.toString());
     }
 }
