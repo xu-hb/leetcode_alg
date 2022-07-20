5d78c4e4a53be0492ecd95480a81a56f98113ec3
prysnail <prysnail@outlook.com>
2022-07-20 20:36:28 +0800

    435.无重叠区间

--- src/main/java/com/alg/algorithms/alg500/Alg440.java
+++ src/main/java/com/alg/algorithms/alg500/Alg440.java
@@ -1,11 +1,32 @@
 package com.alg.algorithms.alg500;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Alg440 {
+
+    /**
+     * 435.无重叠区间
+     * 贪心
+     * @param intervals
+     * @return
+     */
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        //按end升序排序
+        Arrays.sort(intervals , (o1, o2) -> {return o1[1]-o2[1];});
+        int lastEnd = intervals[0][1];
+        int count = 1;
+        for (int i=1;i<n;i++){
+            int start = intervals[i][0];
+            //贪心：取end的最小值
+            if (start>=lastEnd){
+                count++;
+                lastEnd = intervals[i][1];
+            }
+        }
+        return n-count;
+    }
+
     /**
      * 438.找到字符串中所有字母异位词
      * @param s
