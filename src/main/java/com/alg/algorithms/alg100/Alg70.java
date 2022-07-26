a8fe076ef29bbfa5c8417c298da21ca916afebb7
prysnail <prysnail@outlook.com>
2022-07-26 21:58:57 +0800

    62.不同路径

--- src/main/java/com/alg/algorithms/alg100/Alg70.java
+++ src/main/java/com/alg/algorithms/alg100/Alg70.java
@@ -1,6 +1,31 @@
 package com.alg.algorithms.alg100;
 
 public class Alg70 {
+
+    /**
+     * 62.不同路径
+     * DP
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for (int j=0;j<m;j++){
+            dp[j][0]=1;
+        }
+
+        for (int i=1;i<m;i++){
+            for (int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     /**
      * 69. x的平方根
      * 二分查找
