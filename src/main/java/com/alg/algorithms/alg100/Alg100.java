e3adb0443d2f2a6b79821afd699a9ec0be864574
prysnail <prysnail@outlook.com>
2022-07-26 20:25:51 +0800

    96.不同的二叉搜索树

--- src/main/java/com/alg/algorithms/alg100/Alg100.java
+++ src/main/java/com/alg/algorithms/alg100/Alg100.java
@@ -84,6 +84,24 @@ public class Alg100 {
     }
 
     /**
+     * 96.不同的二叉搜索树
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
+    /**
      * 98.验证BST
      * @param root
      * @return
