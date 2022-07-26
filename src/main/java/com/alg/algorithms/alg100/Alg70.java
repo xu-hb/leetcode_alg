8258a458d85e3290a11b8f9c47853715cee19ba1
prysnail <prysnail@outlook.com>
2022-07-26 22:25:25 +0800

    63.不同路径II

--- src/main/java/com/alg/algorithms/alg100/Alg70.java
+++ src/main/java/com/alg/algorithms/alg100/Alg70.java
@@ -27,6 +27,46 @@ public class Alg70 {
     }
 
     /**
+     * 63.不同路径II
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0]==1) return 0;
+
+        int[][] dp = new int[m][n];
+        dp[0][0]=1;
+        for (int i=1;i<n;i++){
+            if (dp[0][i-1]==0 || obstacleGrid[0][i]==1){
+                dp[0][i]=0;
+            }else {
+                dp[0][i]=1;
+            }
+        }
+        for (int j=1;j<m;j++){
+            if (dp[j-1][0]==0 || obstacleGrid[j][0]==1){
+                dp[j][0]=0;
+            }else {
+                dp[j][0]=1;
+            }
+        }
+
+        for (int i=1;i<m;i++){
+            for (int j=1;j<n;j++){
+                if (obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    /**
      * 69. x的平方根
      * 二分查找
      * tag:BinarySearch
