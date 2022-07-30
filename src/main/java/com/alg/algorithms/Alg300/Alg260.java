c361a5fdbc6f86d857e6663aa11ddb0835c82895
prysnail <prysnail@outlook.com>
2022-07-30 22:14:39 +0800

    260.只出现一次的数字 III

---
+++ src/main/java/com/alg/algorithms/Alg300/Alg260.java
@@ -0,0 +1,26 @@
+package com.alg.algorithms.Alg300;
+
+public class Alg260 {
+    /**
+     * 260.只出现一次的数字 III
+     * @param nums
+     * @return
+     */
+    public int[] singleNumber(int[] nums) {
+        int xorsum = 0;
+        for (int num : nums) {
+            xorsum ^= num;
+        }
+        // 防止溢出
+        int lsb = (xorsum == Integer.MIN_VALUE ? xorsum : xorsum & (-xorsum));
+        int type1 = 0, type2 = 0;
+        for (int num : nums) {
+            if ((num & lsb) != 0) {
+                type1 ^= num;
+            } else {
+                type2 ^= num;
+            }
+        }
+        return new int[]{type1, type2};
+    }
+}
