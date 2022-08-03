7a5c2102414ba087a00eab072e971f3c9c4ca7bb
prysnail <prysnail@outlook.com>
2022-08-03 23:09:11 +0800

    [offer]3.数组中重复的数字 4.0

--- src/main/java/com/alg/algorithms/offer/alg50/Alg10.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg10.java
@@ -38,4 +38,46 @@ public class Alg10 {
         }
         return -1;
     }
+
+    /**
+     * 3.数组中重复的数字 3.0
+     * 值作为索引
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     * @param nums
+     * @return
+     */
+    public int findRepeatNumber_3(int[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i=0;i<nums.length;i++){
+            arr[nums[i]]+=1;
+        }
+        for (int i=0;i<nums.length;i++){
+            if (arr[i]>1) return i;
+        }
+        return -1;
+    }
+
+    /**
+     * 3.数组中重复的数字 4.0
+     * 原数组上 值与索引互调
+     * 时间复杂度：O(N)
+     * 空间复杂度:O(1)
+     * @param nums
+     * @return
+     */
+    public int findRepeatNumber_4(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            if(nums[i] == i) {
+                i++;
+                continue;
+            }
+            if(nums[nums[i]] == nums[i]) return nums[i];
+            int tmp = nums[i];
+            nums[i] = nums[tmp];
+            nums[tmp] = tmp;
+        }
+        return -1;
+    }
 }
