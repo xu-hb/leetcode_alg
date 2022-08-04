10cc37c077846f1601a7f791e79ad384ae2a2269
prysnail <prysnail@outlook.com>
2022-08-04 22:36:16 +0800

    1403.非递增顺序的最小子序列 3.0:计数排序

--- src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1410.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1410.java
@@ -5,15 +5,47 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Alg1410 {
+
     /**
      * 1403.非递增顺序的最小子序列 2.0
      * 计数排序
+     * @param nums
+     * @return
+     */
+    public List<Integer> minSubsequence_2(int[] nums){
+        int M = 101;
+        int sum =0;
+        int[] count = new int[M];
+        for (int n: nums){
+            sum+=n;
+            count[n]++;
+        }
+
+        int total=0;
+        int idx= count.length-1;
+        List<Integer> ans = new ArrayList<>();
+
+        while (2*total<=sum){
+            if (count[idx]>0){
+                total+=idx;
+                ans.add(idx);
+                count[idx]--;
+            }else {
+                idx--;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 1403.非递增顺序的最小子序列 3.0
+     * 计数排序--缩小空间
      * 时间复杂度：O(N)
      * 空间复杂度：O(Max(nums))
      * @param nums
      * @return
      */
-    public List<Integer> minSubsequence_2(int[] nums) {
+    public List<Integer> minSubsequence_3(int[] nums) {
         int min = Arrays.stream(nums).min().getAsInt();
         int max = Arrays.stream(nums).max().getAsInt();
         int sum = Arrays.stream(nums).sum();
