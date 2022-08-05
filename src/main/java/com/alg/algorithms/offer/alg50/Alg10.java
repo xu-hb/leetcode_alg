39aa8c1d89280c98168bf6111cd00ffeb5efa435
prysnail <prysnail@outlook.com>
2022-08-05 17:53:25 +0800

    [offer]5.替换空格

--- src/main/java/com/alg/algorithms/offer/alg50/Alg10.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg10.java
@@ -105,4 +105,20 @@ public class Alg10 {
         }
         return false;
     }
+
+    /**
+     * 5.替换空格
+     * @param s
+     * @return
+     */
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<s.length();i++){
+            if (s.charAt(i)==' ')
+                sb.append("%20");
+            else
+                sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
 }
