69b209c361ab3b94b3d7220ef80aa8c6df690f67
prysnail <prysnail@outlook.com>
2022-08-05 14:22:05 +0800

    代码迁移

--- src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1410.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1410.java
@@ -7,6 +7,28 @@ import java.util.List;
 public class Alg1410 {
 
     /**
+     * 1403.非递增顺序的最小子序列
+     * 时间复杂度：O(NlogN)
+     * 空间复杂度: O(logN)
+     * @param nums
+     * @return
+     */
+    public List<Integer> minSubsequence(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        int cal=0;
+        for (int i=nums.length-1;i>=0;i--){
+            cal+=nums[i];
+            list.add(nums[i]);
+            if (cal*2 > sum){
+                return list;
+            }
+        }
+        return list;
+    }
+
+    /**
      * 1403.非递增顺序的最小子序列 2.0
      * 计数排序
      * @param nums
--- src/main/java/com/alg/algorithms/leetcode/alg1600/Alg1510.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1600/Alg1510.java
@@ -5,25 +5,5 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Alg1510 {
-    /**
-     * 1403.非递增顺序的最小子序列
-     * 时间复杂度：O(NlogN)
-     * 空间复杂度: O(logN)
-     * @param nums
-     * @return
-     */
-    public List<Integer> minSubsequence(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
-        List<Integer> list = new ArrayList<>();
-        Arrays.sort(nums);
-        int cal=0;
-        for (int i=nums.length-1;i>=0;i--){
-            cal+=nums[i];
-            list.add(nums[i]);
-            if (cal*2 > sum){
-                return list;
-            }
-        }
-        return list;
-    }
+
 }
