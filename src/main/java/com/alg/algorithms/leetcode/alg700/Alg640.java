2b709e9dbdf35645067e7a51e490ec03ff3db6a0
prysnail <prysnail@outlook.com>
2022-08-07 21:48:37 +0800

    636.函数的独占时间

--- src/main/java/com/alg/algorithms/leetcode/alg700/Alg640.java
+++ src/main/java/com/alg/algorithms/leetcode/alg700/Alg640.java
@@ -2,9 +2,7 @@ package com.alg.algorithms.leetcode.alg700;
 
 import com.alg.common.TreeNode;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Alg640 {
     /**
@@ -119,4 +117,35 @@ public class Alg640 {
         dfs(root.left,d+1);
         dfs(root.right,d+1);
     }
+
+    /**
+     * 636.函数的独占时间
+     * @param n
+     * @param logs
+     * @return
+     */
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Deque<int[]> stack = new ArrayDeque<int[]>(); // {idx, 开始运行的时间}
+        int[] res = new int[n];
+        for (String log : logs) {
+            int idx = Integer.parseInt(log.substring(0, log.indexOf(':')));
+            String type = log.substring(log.indexOf(':') + 1, log.lastIndexOf(':'));
+            int timestamp = Integer.parseInt(log.substring(log.lastIndexOf(':') + 1));
+            if ("start".equals(type)) {
+                if (!stack.isEmpty()) {
+                    res[stack.peek()[0]] += timestamp - stack.peek()[1];
+                    stack.peek()[1] = timestamp;
+                }
+                stack.push(new int[]{idx, timestamp});
+            } else {
+                int[] t = stack.pop();
+                res[t[0]] += timestamp - t[1] + 1;
+                if (!stack.isEmpty()) {
+                    stack.peek()[1] = timestamp + 1;
+                }
+            }
+        }
+        return res;
+    }
+
 }
