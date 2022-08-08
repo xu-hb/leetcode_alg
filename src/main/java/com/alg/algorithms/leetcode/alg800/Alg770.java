09367718604396b6dde8bac3987165581443ee6e
prysnail <prysnail@outlook.com>
2022-08-08 21:33:40 +0800

    761.特殊的二进制序列

---
+++ src/main/java/com/alg/algorithms/leetcode/alg800/Alg770.java
@@ -0,0 +1,29 @@
+package com.alg.algorithms.leetcode.alg800;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Alg770 {
+    /**
+     * 761.特殊的二进制序列
+     * @param s
+     * @return
+     */
+    public String makeLargestSpecial(String s) {
+        if (s.length() == 0) return s;
+        List<String> list = new ArrayList<>();
+        char[] cs = s.toCharArray();
+        for (int i = 0, j = 0, k = 0; i < cs.length; i++) {
+            k += cs[i] == '1' ? 1 : -1;
+            if (k == 0) {
+                list.add("1" + makeLargestSpecial(s.substring(j + 1, i)) + "0");
+                j = i + 1;
+            }
+        }
+        Collections.sort(list, (a, b)->(b + a).compareTo(a + b));
+        StringBuilder sb = new StringBuilder();
+        for (String str : list) sb.append(str);
+        return sb.toString();
+    }
+}
