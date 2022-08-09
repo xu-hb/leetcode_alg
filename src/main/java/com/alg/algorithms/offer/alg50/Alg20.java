1cd10a0a1bcc00b96e301ba6f7e3f14b87dd2073
prysnail <prysnail@outlook.com>
2022-08-09 22:42:49 +0800

    [offer]11.旋转数组的最小数字 2.0

--- src/main/java/com/alg/algorithms/offer/alg50/Alg20.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg20.java
@@ -15,4 +15,29 @@ public class Alg20 {
         }
         return numbers[0];
     }
+
+    /**
+     * 11.旋转数组的最小数字 2.0
+     * 时间复杂度：O(logN)
+     * 空间复杂度：O(1)
+     * @param numbers
+     * @return
+     */
+    public int minArray_2(int[] numbers) {
+        int n = numbers.length;
+        int l=0,r=n-1;
+        while (l<r){
+            int mid = l + (r-l)/2;
+            //和右端点值比较，因为左端点不能区分出递增情况
+            if (numbers[mid]>numbers[r]){
+                l=mid+1;
+            }else if (numbers[mid]<numbers[r]){
+                r=mid;
+            }else {
+                //此时不能判定最小值在那个区间，缩小区间范围
+                r=r-1;
+            }
+        }
+        return numbers[r];
+    }
 }
