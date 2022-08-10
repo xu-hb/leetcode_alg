a7cfecba2bb128eefb0ea9ed121fd709202187ac
prysnail <prysnail@outlook.com>
2022-08-10 14:30:08 +0800

    [offer]14-I.剪绳子

--- src/main/java/com/alg/algorithms/offer/alg50/Alg20.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg20.java
@@ -85,4 +85,22 @@ public class Alg20 {
             return false;
         }
     }
+
+    /**
+     * 14-I.剪绳子
+     * dp
+     * @param n
+     * @return
+     */
+    public int cuttingRope(int n) {
+        //dp：k-绳子长度;v-最大乘积
+        int[]dp = new int[n+1];
+        dp[2] = 1;
+        for (int i=3;i<=n;i++){
+            for (int j=1;j<i;j++){
+                dp[i] = Math.max(dp[i],Math.max(j*dp[i-j],j*(i-j)));
+            }
+        }
+        return dp[n];
+    }
 }
