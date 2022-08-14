e17ff70252ac01e2f99d061f1c9742c5e021c4f1
prysnail <prysnail@outlook.com>
2022-08-14 18:34:45 +0800

    [offer]25.合并两个排序的链表

--- src/main/java/com/alg/algorithms/offer/alg50/Alg30.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg30.java
@@ -47,4 +47,34 @@ public class Alg30 {
 
         return pre;
     }
+
+    /**
+     * 25.合并两个排序的链表
+     * 时间复杂度：O(N+M)
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode();
+        ListNode cursor = dummyNode;
+
+        while (l1 !=null && l2 !=null){
+            if (l1.val<=l2.val){
+                cursor.next=l1;
+                l1=l1.next;
+            }else {
+                cursor.next=l2;
+                l2=l2.next;
+            }
+            cursor = cursor.next;
+        }
+        if (l1 !=null){
+            cursor.next=l1;
+        }
+        if (l2 !=null){
+            cursor.next=l2;
+        }
+        return dummyNode.next;
+    }
 }
