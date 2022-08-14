54ae659ad9a09176b77eaa0920f062664fa39c75
prysnail <prysnail@outlook.com>
2022-08-14 22:08:09 +0800

    [offer]28.对称的二叉树

--- src/main/java/com/alg/algorithms/offer/alg50/Alg30.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg30.java
@@ -193,4 +193,21 @@ public class Alg30 {
         }
         return root;
     }
+
+    /**
+     * 28.对称的二叉树
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root , root);
+    }
+
+    private boolean isSymmetric(TreeNode root1, TreeNode root2) {
+        if (root1==null && root2==null)
+            return true;
+        if (root1==null || root2==null)
+            return false;
+        return root1.val == root2.val && isSymmetric(root1.left,root2.right) && isSymmetric(root1.right,root2.left);
+    }
 }
