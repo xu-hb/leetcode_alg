67fb2430cd4a417d06049b18e9604beecba63dfc
prysnail <prysnail@outlook.com>
2022-08-14 18:43:10 +0800

    [offer]25.合并两个排序的链表 2.0：递归

--- src/main/java/com/alg/algorithms/offer/alg50/Alg30.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg30.java
@@ -77,4 +77,26 @@ public class Alg30 {
         }
         return dummyNode.next;
     }
+
+    /**
+     * 25.合并两个排序的链表 2.0
+     * 递归
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists_2(ListNode l1, ListNode l2) {
+        if (l1==null)
+            return l2;
+        if (l2==null)
+            return l1;
+
+        if (l1.val<=l2.val){
+            l1.next = mergeTwoLists_2(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists_2(l1,l2.next);
+            return l2;
+        }
+    }
 }
