2693c28184da0ae0b7566e695a7fd9dc09e74071
prysnail <prysnail@outlook.com>
2022-08-18 23:10:10 +0800

    [offer]45.把数组排成最小的数

--- src/main/java/com/alg/algorithms/offer/alg50/Alg50.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg50.java
@@ -1,5 +1,8 @@
 package com.alg.algorithms.offer.alg50;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Alg50 {
     /**
      * 42.连续子数组的最大和
@@ -20,5 +23,21 @@ public class Alg50 {
         return max;
     }
 
-
+    /**
+     * 45.把数组排成最小的数
+     * 时间复杂度：O(NlogN)
+     * 空间复杂度：O(N)
+     * @param nums
+     * @return
+     */
+    public String minNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            strs[i] = String.valueOf(nums[i]);
+        Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
+    }
 }
