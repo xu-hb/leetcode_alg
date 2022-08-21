f7f52a43adf773a552fe400d8d246e1ff45f9454
prysnail <prysnail@outlook.com>
2022-08-21 12:58:54 +0800

    [offer]43.1~n整数中1出现的次数 2.0:时间优化

--- src/main/java/com/alg/algorithms/offer/alg50/Alg50.java
+++ src/main/java/com/alg/algorithms/offer/alg50/Alg50.java
@@ -48,6 +48,27 @@ public class Alg50 {
     }
 
     /**
+     * 43.1~n整数中1出现的次数 2.0
+     * 时间复杂度：O(logN)
+     * @param n
+     * @return
+     */
+    public int countDigitOne_2(int n) {
+        int digit = 1, res = 0;
+        int high = n / 10, cur = n % 10, low = 0;
+        while(high != 0 || cur != 0) {
+            if(cur == 0) res += high * digit;
+            else if(cur == 1) res += high * digit + low + 1;
+            else res += (high + 1) * digit;
+            low += cur * digit;
+            cur = high % 10;
+            high /= 10;
+            digit *= 10;
+        }
+        return res;
+    }
+
+    /**
      * 45.把数组排成最小的数
      * 时间复杂度：O(NlogN)
      * 空间复杂度：O(N)
