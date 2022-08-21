07cd009311c245a1442639fc7f36af77a9b48882
prysnail <prysnail@outlook.com>
2022-08-21 21:19:26 +0800

    [offer]53-II.0~n-1中缺失的数字

--- src/main/java/com/alg/algorithms/offer/alg100/Alg60.java
+++ src/main/java/com/alg/algorithms/offer/alg100/Alg60.java
@@ -75,14 +75,15 @@ public class Alg60 {
         int cnt = 0;
 
         int l=0,r=nums.length-1;
-        while (l<r){
+        //左边界
+        while (l<=r){
             int middle = l + (r-l)/2;
             if (target>nums[middle]){
                 l=middle+1;
             }else if (target<nums[middle]){
                 r=middle-1;
             }else {
-                r=middle;
+                r=middle-1;
             }
         }
         while (l< nums.length && nums[l]==target){
@@ -90,5 +91,23 @@ public class Alg60 {
             l++;
         }
         return cnt;
+    }
+
+    /**
+     * 53-II.0~n-1中缺失的数字
+     * @param nums
+     * @return
+     */
+    public int missingNumber(int[] nums) {
+        int l =0,r=nums.length-1;
+        while (l<=r){
+            int mid = l+(r-l)/2;
+            if (mid==nums[mid]){
+                l=mid+1;
+            }else if (mid<nums[mid]){
+                r=mid-1;
+            }
+        }
+        return l;
     }
 }
