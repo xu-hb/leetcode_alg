23e07286c1ccebb6e02341b52b550e08f062404a
prysnail <prysnail@outlook.com>
2022-08-22 17:33:41 +0800

    655.输出二叉树

--- src/main/java/com/alg/algorithms/leetcode/alg700/Alg660.java
+++ src/main/java/com/alg/algorithms/leetcode/alg700/Alg660.java
@@ -2,10 +2,7 @@ package com.alg.algorithms.leetcode.alg700;
 
 import com.alg.common.TreeNode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Alg660 {
 
@@ -99,5 +96,52 @@ public class Alg660 {
         //构建右子树
         node.right = buildTree(nums , maxIndex+1 , end);
         return node;
+    }
+
+    /**
+     * 655.输出二叉树
+     * @param root
+     * @return
+     */
+    public List<List<String>> printTree(TreeNode root) {
+        //获取高度
+        int height = getHeight(root);
+        String[][] ans = new String[height][(1 << height) - 1];
+        for (String[] arr : ans) {
+            Arrays.fill(arr, "");
+        }
+        List<List<String>> res = new ArrayList<>();
+        fill(ans, root, 0, 0, ans[0].length);
+        for (String[] arr : ans) {
+            res.add(Arrays.asList(arr));
+        }
+        return res;
+    }
+    /**
+     * 填充数组
+     * @param ans  数组
+     * @param root 根节点
+     * @param i    第几行
+     * @param l    左节点
+     * @param r    右节点
+     */
+    private void fill(String[][] ans, TreeNode root, int i, int l, int r) {
+        if (root == null) {
+            return;
+        }
+        ans[i][(l + r) / 2] = "" + root.val;
+        fill(ans, root.left, i + 1, l, (l + r) / 2);
+        fill(ans, root.right, i + 1, (l + r + 1) / 2, r);
+    }
+    /**
+     * 获取树的高度
+     * @param root
+     * @return
+     */
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
     }
 }
