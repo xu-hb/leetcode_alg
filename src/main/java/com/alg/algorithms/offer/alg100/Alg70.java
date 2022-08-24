6d88132db9ff40322bd5241fda27444f0537be1f
prysnail <prysnail@outlook.com>
2022-08-24 11:00:13 +0800

    [offer]61.扑克牌中的顺子

--- src/main/java/com/alg/algorithms/offer/alg100/Alg70.java
+++ src/main/java/com/alg/algorithms/offer/alg100/Alg70.java
@@ -3,6 +3,30 @@ package com.alg.algorithms.offer.alg100;
 import com.alg.common.TreeNode;
 
 public class Alg70 {
+
+    /**
+     * 61.扑克牌中的顺子
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(C)
+     * @param nums
+     * @return
+     */
+    public boolean isStraight(int[] nums) {
+        int[] repeat = new int[14];
+        int max=0,min=16;
+        for (int n : nums){
+            if (n==0) {
+                continue;
+            }
+            max = Math.max(max,n);
+            min = Math.min(min,n);
+            if (repeat[n]!=0)
+                return false;
+            repeat[n]++;
+        }
+        return max-min<5;
+    }
+
     /**
      * 68-I.二叉搜索树的最近公共祖先
      * @param root
