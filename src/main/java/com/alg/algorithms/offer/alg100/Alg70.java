3579ff334199c52de4652bbc956176a2aa90a9f4
prysnail <prysnail@outlook.com>
2022-08-24 12:35:45 +0800

    [offer]63.股票的最大利润

--- src/main/java/com/alg/algorithms/offer/alg100/Alg70.java
+++ src/main/java/com/alg/algorithms/offer/alg100/Alg70.java
@@ -50,6 +50,27 @@ public class Alg70 {
     }
 
     /**
+     * 63.股票的最大利润
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices)  {
+        if(prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int[]dp=new int[prices.length];
+        dp[0]=0;
+        int min=prices[0];
+        for (int i=1;i<prices.length;i++){
+            if (prices[i]<=min){
+                min = prices[i];
+            }
+            dp[i]=Math.max(dp[i-1],prices[i]-min);
+        }
+        return dp[prices.length-1];
+    }
+
+    /**
      * 68-I.二叉搜索树的最近公共祖先
      * @param root
      * @param p
