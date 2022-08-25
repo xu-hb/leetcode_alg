744e152e8b34d45fc6d985cff12092e65260c194
prysnail <prysnail@outlook.com>
2022-08-25 15:23:05 +0800

    658.找到 K 个最接近的元素

--- src/main/java/com/alg/algorithms/leetcode/alg700/Alg660.java
+++ src/main/java/com/alg/algorithms/leetcode/alg700/Alg660.java
@@ -3,6 +3,9 @@ package com.alg.algorithms.leetcode.alg700;
 import com.alg.common.TreeNode;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Alg660 {
 
@@ -144,4 +147,46 @@ public class Alg660 {
         }
         return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
     }
+
+    /**
+     * 658.找到 K 个最接近的元素
+     * @param arr
+     * @param k
+     * @param x
+     * @return
+     */
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int right = binarySearch(arr, x);
+        int left = right - 1;
+        while (k-- > 0) {
+            if (left < 0) {
+                right++;
+            } else if (right >= arr.length) {
+                left--;
+            } else if (x - arr[left] <= arr[right] - x) {
+                left--;
+            } else {
+                right++;
+            }
+        }
+        List<Integer> ans = new ArrayList<Integer>();
+        for (int i = left + 1; i < right; i++) {
+            ans.add(arr[i]);
+        }
+        return ans;
+    }
+
+    public int binarySearch(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
 }
