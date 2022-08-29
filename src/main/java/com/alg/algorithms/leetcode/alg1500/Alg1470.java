1011c884f42d56d0ef22a08b96bed7fff6547bac
prysnail <prysnail@outlook.com>
2022-08-29 10:16:48 +0800

    1470.重新排列数组

--- src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1470.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1470.java
@@ -20,4 +20,22 @@ public class Alg1470 {
         }
         return (a-1)*(b-1);
     }
+
+    /**
+     * 1470.重新排列数组
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     * @param nums
+     * @param n
+     * @return
+     */
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2*n];
+        int idx=0;
+        for (int i=0;i<n;i++){
+            ans[idx++] = nums[i];
+            ans[idx++] = nums[i+n];
+        }
+        return ans;
+    }
 }
