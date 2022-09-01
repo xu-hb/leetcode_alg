a754ee2c674db0874a4229fb09ce9e3ee60731d6
prysnail <prysnail@outlook.com>
2022-09-01 21:18:11 +0800

    1475.商品折扣后的最终价格 2.0:单调栈

--- src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1480.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1480.java
@@ -1,5 +1,11 @@
 package com.alg.algorithms.leetcode.alg1500;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class Alg1480 {
     /**
      * 1475.商品折扣后的最终价格
@@ -19,6 +25,28 @@ public class Alg1480 {
                 }
             }
         }
+        return ret;
+    }
+
+    /**
+     * 1475.商品折扣后的最终价格 2.0
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     * @param prices
+     * @return
+     */
+    public int[] finalPrices_2(int[] prices){
+        int[] ret = new int[prices.length];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i=prices.length-1;i>=0;i--){
+            while (! stack.isEmpty() && stack.peek()>prices[i]){
+                stack.pop();
+            }
+            ret[i] = stack.isEmpty() ? prices[i] : prices[i]-stack.peek();
+            stack.push(prices[i]);
+        }
+
         return ret;
     }
 }
