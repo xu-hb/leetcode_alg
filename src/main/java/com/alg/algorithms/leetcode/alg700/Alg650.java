a35ce1ce20e1b84fa67cd3745c0e7f18782e7d26
prysnail <prysnail@outlook.com>
2022-09-03 16:02:33 +0800

    646.最长数对链

--- src/main/java/com/alg/algorithms/leetcode/alg700/Alg650.java
+++ src/main/java/com/alg/algorithms/leetcode/alg700/Alg650.java
@@ -3,6 +3,8 @@ package com.alg.algorithms.leetcode.alg700;
 import com.alg.design.deque.MyCircularDeque;
 import com.alg.design.tree.Trie;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Alg650 {
@@ -15,6 +17,27 @@ public class Alg650 {
         MyCircularDeque circularDeque = new MyCircularDeque(5);
         circularDeque.insertFront(4);
         return circularDeque;
+    }
+
+    /**
+     * 646.最长数对链
+     * 时间复杂度：O(NlogN)
+     * 空间复杂度：O(logN)
+     * @param pairs
+     * @return
+     */
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, Comparator.comparingInt(o -> o[1]));
+
+        int cnt=1;
+        int l1=pairs[0][1];
+        for (int i=1;i< pairs.length;i++){
+            if (pairs[i][0]>l1){
+                cnt++;
+                l1=pairs[i][1];
+            }
+        }
+        return cnt;
     }
 
     /**
