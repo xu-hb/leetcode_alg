e1c59a39d97b84645384eaa20a457727a55646f5
prysnail <prysnail@outlook.com>
2022-09-08 22:22:46 +0800

    667.优美的排列II

--- src/main/java/com/alg/algorithms/leetcode/alg700/Alg670.java
+++ src/main/java/com/alg/algorithms/leetcode/alg700/Alg670.java
@@ -66,6 +66,23 @@ public class Alg670 {
     }
 
     /**
+     * 667.优美的排列II
+     * @param n
+     * @param k
+     * @return
+     */
+    public int[] constructArray(int n, int k) {
+        int[] ans = new int[n];
+        int t = n - k - 1;
+        for (int i = 0; i < t; i++) ans[i] = i + 1;
+        for (int i = t, a = n - k, b = n; i < n; ) {
+            ans[i++] = a++;
+            if (i < n) ans[i++] = b--;
+        }
+        return ans;
+    }
+
+    /**
      * 670.最大交换
      * @param num
      * @return
