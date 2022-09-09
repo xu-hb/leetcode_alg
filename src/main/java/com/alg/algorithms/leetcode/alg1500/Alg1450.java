ee3363bcd031551c307c58dc13d40d98ead5f178
prysnail <prysnail@outlook.com>
2022-09-09 17:52:11 +0800

    1446.连续字符

--- src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1450.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1450.java
@@ -1,6 +1,30 @@
 package com.alg.algorithms.leetcode.alg1500;
 
 public class Alg1450 {
+
+    /**
+     * 1446.连续字符
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     * @param s
+     * @return
+     */
+    public int maxPower(String s) {
+        int max = 0,cnt=0;
+        int l=0,r=0;
+        while (r<s.length()){
+            if (s.charAt(l)==s.charAt(r)){
+                cnt++;
+                r++;
+            }else {
+                cnt=0;
+                l=r;
+            }
+            max = Math.max(max , cnt);
+        }
+        return max;
+    }
+
     /**
      * 1450.在既定时间作业的学生人数
      * @param startTime
@@ -18,20 +42,4 @@ public class Alg1450 {
         return cnt;
     }
 
-    /**
-     * 1455.检查单词是否为句中其他单词的前缀
-     * 时间复杂度：O(n*m)
-     * @param sentence
-     * @param searchWord
-     * @return
-     */
-    public int isPrefixOfWord(String sentence, String searchWord) {
-        String[] arr = sentence.split(" ");
-        for (int i=0;i<arr.length;i++){
-            if (arr[i].startsWith(searchWord)){
-                return i+1;
-            }
-        }
-        return -1;
-    }
 }
