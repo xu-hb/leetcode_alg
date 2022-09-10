d67097f7fddfd95e879a88b7659bfc6d11d6866b
prysnail <prysnail@outlook.com>
2022-09-10 20:40:46 +0800

    669.修剪二叉搜索树

--- src/main/java/com/alg/algorithms/leetcode/alg700/Alg670.java
+++ src/main/java/com/alg/algorithms/leetcode/alg700/Alg670.java
@@ -83,6 +83,29 @@ public class Alg670 {
     }
 
     /**
+     * 669.修剪二叉搜索树
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root==null)
+            return null;
+
+        if (root.val< low)
+            return trimBST(root.right,low,high);
+        if (root.val > high)
+            return trimBST(root.left,low,high);
+
+        root.left = trimBST(root.left,low,high);
+        root.right = trimBST(root.right,low,high);
+        return root;
+    }
+
+    /**
      * 670.最大交换
      * @param num
      * @return
