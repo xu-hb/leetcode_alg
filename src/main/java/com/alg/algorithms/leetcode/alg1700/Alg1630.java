0f981bec969324e23918a00cb4d7c6f3ac5ef243
prysnail <prysnail@outlook.com>
2022-09-17 20:46:00 +0800

    1624.两个相同字符之间的最长子字符串

--- src/main/java/com/alg/algorithms/leetcode/alg1700/Alg1630.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1700/Alg1630.java
@@ -1,6 +1,31 @@
 package com.alg.algorithms.leetcode.alg1700;
 
+import java.util.Arrays;
+
 public class Alg1630 {
+
+    /**
+     * 1624.两个相同字符之间的最长子字符串
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(C)
+     * @param s
+     * @return
+     */
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int[] loc = new int[26];
+        Arrays.fill(loc,-1);
+
+        int ret=-1;
+        for (int i=0;i<s.length();i++){
+            if (loc[s.charAt(i)-'a'] !=-1){
+                ret = Math.max(ret,i-loc[s.charAt(i)-'a']-1);
+            }else {
+                loc[s.charAt(i)-'a']=i;
+            }
+        }
+        return ret;
+    }
+
     /**
      * 1629.按键持续时间最长的键
      * 时间复杂度：O(N)
