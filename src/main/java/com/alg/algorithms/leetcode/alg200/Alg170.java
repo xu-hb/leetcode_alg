94b94ed2ce3218f60e2e6fd10bcd83f7eb483671
prysnail <prysnail@outlook.com>
2022-09-20 22:04:15 +0800

    162.寻找峰值

--- src/main/java/com/alg/algorithms/leetcode/alg200/Alg170.java
+++ src/main/java/com/alg/algorithms/leetcode/alg200/Alg170.java
@@ -1,6 +1,29 @@
 package com.alg.algorithms.leetcode.alg200;
 
 public class Alg170 {
+
+    /**
+     * 162.寻找峰值
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     * @param nums
+     * @return
+     */
+    public int findPeakElement(int[] nums) {
+        for (int i=1;i<nums.length-1;i++){
+            if (nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+        //处理边界
+        if (nums.length==1) return 0;
+        if (nums.length>1){
+            if (nums[0]>nums[1]) return 0;
+            if (nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
+        }
+        return -1;
+    }
+
     /**
      * 167.两数之和II-输入有序数组
      * @param numbers
