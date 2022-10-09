dd5970d4fa178650e3c0903b3a910a5c2dc767a8
xuhb <hongbing.xu@outlook.com>
2022-10-09 10:09:58 +0800

    856.括号的分数

--- src/main/java/com/alg/algorithms/leetcode/alg900/Alg860.java
+++ src/main/java/com/alg/algorithms/leetcode/alg900/Alg860.java
@@ -1,6 +1,34 @@
 package com.alg.algorithms.leetcode.alg900;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Alg860 {
+
+    /**
+     * 856.括号的分数
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     * @param s
+     * @return
+     */
+    public int scoreOfParentheses(String s) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        //初始默认值0
+        deque.push(0);
+        for (int i=0;i<s.length();i++){
+            if (s.charAt(i)=='(')
+                deque.push(0);
+            else {
+                int v = deque.pop();
+                //AB=A+B在初始值为0时直接相加；()和(A)统一处理为：Math.max(2*v,1)
+                int res = deque.pop() + Math.max(2*v,1);
+                deque.push(res);
+            }
+        }
+        return deque.peek();
+    }
+
     /**
      * 860.柠檬水找零
      * @param bills
