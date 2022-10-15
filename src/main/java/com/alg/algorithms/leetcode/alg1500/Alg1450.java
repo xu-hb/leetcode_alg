52bfa46afaf39bb54b1eb413d6a3ad155388216c
xuhb <hongbing.xu@outlook.com>
2022-10-15 19:53:45 +0800

    1441.用栈操作构建数组

--- src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1450.java
+++ src/main/java/com/alg/algorithms/leetcode/alg1500/Alg1450.java
@@ -1,6 +1,41 @@
 package com.alg.algorithms.leetcode.alg1500;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Alg1450 {
+
+    /**
+     * 1441.用栈操作构建数组
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     * @param target
+     * @param n
+     * @return
+     */
+    public List<String> buildArray(int[] target, int n) {
+        int tail = target[target.length-1];
+        n = Math.min(tail,n);
+
+        String push = "Push";
+        String pop = "Pop";
+        List<String> ret = new ArrayList<>();
+
+        int cur=0;
+        int tIdx=0;
+        while (cur<n){
+            if (target[tIdx]==cur+1){
+                ret.add(push);
+                tIdx++;
+            }else {
+                ret.add(push);
+                ret.add(pop);
+            }
+            cur++;
+        }
+
+        return ret;
+    }
 
     /**
      * 1446.连续字符
