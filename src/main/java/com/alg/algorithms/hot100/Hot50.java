e7a69296c6991e8c20714f077602d8260034e335
xuhb <hongbing.xu@outlook.com>
2022-12-15 23:18:12 +0800

    [hot100] alg:11

--- src/main/java/com/alg/algorithms/hot100/Hot50.java
+++ src/main/java/com/alg/algorithms/hot100/Hot50.java
@@ -90,5 +90,93 @@ public class Hot50 {
         return max;
     }
 
+    /**
+     * 5.最长回文子串
+     * 中心扩散
+     * 时间复杂度：O(N^2)
+     * 空间复杂度：O(1)
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int l=0,r=0;
+        for (int i=0;i<n;i++){
+            int len1 = spread(s,i,i+1);
+            int len2 = spread(s,i,i);
+            int len = Math.max(len1,len2);
+            if (len>r-l+1){
+                if (len1>=len2){
+                    l=1+i-len/2;
+                    r=i+len/2;
+                }else {
+                    l=i-len/2;
+                    r=i+len/2;
+                }
+            }
+        }
+        return s.substring(l,r+1);
+    }
+
+    /**
+     * 5.最长回文子串 2.0
+     * DP
+     * @param s
+     * @return
+     */
+    public String longestPalindrome_2(String s){
+        int n = s.length();
+        if (n==1) return s;
+        //dp:s从i~j的子串是否为回文串
+        boolean dp[][] = new boolean[n][n];
+        int l=0,r=0;
+
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                if (s.charAt(j)==s.charAt(i) && (dp[j+1][i-1] || i-j+1<=3)){
+                    dp[j][i]=true;
+                    if (i-j>r-l){
+                        l=j;
+                        r=i;
+                    }
+                }
+            }
+        }
+        return s.substring(l,r+1);
+    }
+
+    private int spread(String s, int l , int r){
+        while (l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
+        }
+        return (r-1)-(l+1)+1;
+    }
+
+    /**
+     * 11.盛最多水的容器
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(1)
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int l=0,r=height.length-1;
+        int max = 0;
+        while(l<r){
+            int area = (r-l)*Math.min(height[l],height[r]);
+            max = Math.max(max,area);
+            if(height[l]<height[r]){
+                int curL=l+1;
+                while (height[curL]<=height[l] && curL<r) curL++;
+                l=curL;
+            }else{
+                int curR=r-1;
+                while (height[curR]<=height[r] && curR>l) curR--;
+                r=curR;
+            }
+        }
+        return max;
+    }
 
 }
