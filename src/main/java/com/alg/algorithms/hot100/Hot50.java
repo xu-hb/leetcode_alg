2801243b764eb6388ead4bff0b17c8c05942c8f4
xuhb <hongbing.xu@outlook.com>
2024-03-07 11:24:04 +0800

    [hot100] 560.和为k的子数组（good）

--- src/main/java/com/alg/algorithms/hot100/Hot50.java
+++ src/main/java/com/alg/algorithms/hot100/Hot50.java
@@ -318,4 +318,25 @@ public class Hot50 {
             fast++;
         }
     }
+
+    /**
+     * 560.和为k的子数组（good）
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum(int[] nums, int k) {
+        int result=0,sum=0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);   //sum-k=0,+1
+        for (int i=0;i<nums.length;i++){
+            sum += nums[i];
+
+            result+=map.getOrDefault(sum-k,0);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return result;
+    }
 }
